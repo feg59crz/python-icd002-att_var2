6ceb3f392b276c73955afeecbb5d06f1fa21537a
Fernando Gomes <fernandogomescruz@gmail.com>
2023-03-13 16:04:29 -0300

    ok

---
+++ README.md
@@ -0,0 +1,12 @@
+# att-var2
+Exemplo de média de notas em Python
+
+## From
+Faculdade de Ciência de Dados.
+
+Matéria: ICD002 Algoritmos e Introdução à Computação
+
+Apostila: Atualização de variáveis + condicionais if e if else - IV
+
+Environment:
+- conda - spyder
---
+++ main.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 10 09:57:55 2023
+
+@author: fernando.cruz7
+"""
+
+media = 0.0
+portugues = 8.0
+matematica = 6.0
+historia = 7.5
+
+print(f"Valor da média: {media}")
+media = (portugues + matematica + historia)/3
+
+print(f"Valor da média: {media}")
+
+print("\n\n")
+
+media = (portugues + matematica) / 2
+
+print(f"Valor da média: {media}")
